7719ef76044cc69f3bc2b3cc797f8a414d25ed58
saipriya-code <60016051+saipriya-code@users.noreply.github.com>
2020-05-04 13:21:22 -0400

    Update AdminLogin.java

--- mmp/src/main/java/adminLogin/AdminLogin.java
+++ mmp/src/main/java/adminLogin/AdminLogin.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
+// login as admin
 public class AdminLogin {
 	WebDriver driver;
 
